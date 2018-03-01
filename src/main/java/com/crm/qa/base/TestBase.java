5364310616900e264f19459ea2524291534615aa
sorowar230 <sorowar230@gmail.com>
2018-03-01 09:37:49 -0500

    changes

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -27,7 +27,7 @@ public class TestBase {
 		
 		try {
 				prop = new Properties();
-				FileInputStream ip = new FileInputStream(".\\src\\main\\java\\com\\crm\\qa\\config\\config.properties");
+				FileInputStream ip = new FileInputStream("..\\src\\main\\java\\com\\crm\\qa\\config\\config.properties");
 				prop.load(ip);
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
